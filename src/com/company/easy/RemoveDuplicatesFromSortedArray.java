b376c2e8e2bfee385be4d47a5032e8120ad70f84
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-01-19 15:55:09 -0500

    RemoveDuplicatesFromSortedArray

--- src/com/company/easy/RemoveDuplicatesFromSortedArray.java
+++ src/com/company/easy/RemoveDuplicatesFromSortedArray.java
@@ -1,4 +1,24 @@
 package com.company.easy;
 
 public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+       // int[] nums = {1,1,2,3};
+        ;
+
+        System.out.println(removeDuplicates(nums));
+    }
+
+    private static int removeDuplicates(int[] nums) {
+        int index=1;
+        for(int i=0; i<nums.length -1; i++) {
+            if(nums[i] !=nums[i+1]) {
+                nums[index] = nums[i+1];
+                index++;
+            }
+        }
+        return index;
+    }
 }
