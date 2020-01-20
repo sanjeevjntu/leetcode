13ec0ff25362931d61cedc5e8584206b331183be
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-01-20 00:46:47 -0500

    initial commit

--- src/com/company/easy/Anagram.java
+++ src/com/company/easy/Anagram.java
@@ -1,5 +1,6 @@
 package com.company.easy;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +8,7 @@ public class Anagram {
 
     public static void main(String[] args) {
         System.out.println(isAnagram("sanjeev", "eevsanj"));
+        System.out.println(isAnagram2("sanjeev", "eevsanj"));
     }
 
     public static boolean isAnagram(String s, String t) {
@@ -23,5 +25,19 @@ public class Anagram {
             }
         }
         return map.values().stream().allMatch(integer -> integer == 0);
+    }
+
+    public static boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] str1 = s.toCharArray();
+        char[] str2= t.toCharArray();
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        return Arrays.equals(str1, str2);
     }
 }
