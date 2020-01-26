267acb7903584dc758f5966ada9a45a911659bb5
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-01-25 20:25:08 -0500

    TOpk elements to be printed

--- src/com/company/easy/Palindrome.java
+++ src/com/company/easy/Palindrome.java
@@ -8,25 +8,23 @@ public class Palindrome {
 
     public static boolean isPalindrome(String s) {
 
-        if(s == null) {
+        if (s == null) {
             return false;
         }
+        int left = 0, right = s.length() - 1;
 
-        int left=0, right=s.length()-1;
-        while(left < right) {
-            if(!Character.isLetterOrDigit(s.charAt(left))) {
-                ++left;
-            }
-            else if(!Character.isLetterOrDigit(s.charAt(right))) {
-                --right;
-            } else if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))              {
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            } else if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            } else if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             } else {
-                ++left;
-                --right;
+                left++;
+                right--;
             }
         }
-
         return true;
     }
 }
---
+++ src/com/company/easy/TopKFrequentElements.java
@@ -0,0 +1,32 @@
+package com.company.easy;
+
+import java.util.*;
+
+public class TopKFrequentElements {
+
+    public static void main(String[] args) {
+
+        System.out.println(topKFrequent(new int[]{4, 1, -1, 2, -1, 2, 3}, 2));
+    }
+
+    public static List<Integer> topKFrequent(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        // // init heap 'the less frequent element first'
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(map::get).reversed());
+
+        //insertion logic with count
+        map.forEach((key, value) -> priorityQueue.offer(value));
+
+        List<Integer> result = new ArrayList<>();
+        while (k-- > 0) {
+            result.add(priorityQueue.poll());
+        }
+        return result;
+
+    }
+}
